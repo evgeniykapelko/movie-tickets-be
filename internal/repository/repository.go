884c0da48527d3ae03c235833397f2c6c9159236
Eugene <softwarearchitect19@gmail.com>
2024-06-29 20:03:31 +0300

    Insert movie.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -15,4 +15,6 @@ type DatabaseRepo interface {
 	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
 
 	AllGenres() ([]*models.Genre, error)
+
+	InsertMovie(movie *models.Movie) (int, error)
 }
